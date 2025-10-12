4e0ff2d2fa7895d973cba73decb707d41484d0cd
Aarjankaini <129028859+Hummann@users.noreply.github.com>
2025-10-12 22:10:36 +0545

    Add files via upload

---
+++ New folder (5)/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+
+    int num[]= {2, 3, 5};
+    int size = sizeof(num)/4;
+    // int smallest = INT_MAX;
+    // for(int i = 0; i < size;i++){
+    //     if (num[i]<smallest){
+    //         smallest = i;
+    //         cout << smallest<<endl;
+    //     }
+    // }
+    //TWO POINTER
+    // int start,end;
+    // start = 0;
+    // end = size -1;
+    // for(start = 0 ; start <end ;start++,end--){
+    //     swap(num[start] ,num[end]);
+
+    // }
+    // for(int i=0;i<size;i++){
+    //     cout << num[i];
+
+    // }
+}
+
